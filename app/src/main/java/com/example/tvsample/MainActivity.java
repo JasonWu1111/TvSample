b4c37aba9a44c11eae995b098c7ade37768dc392
JasonWu <jasonwu19960210@gmail.com>
2017-12-27 20:19:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tvsample/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.tvsample;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+
+import com.example.tvsample.Base.BaseActivity;
+import com.flyco.tablayout.SlidingTabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.tab_layout)
+    SlidingTabLayout tabLayout;
+    @BindView(R.id.view_pager)
+    ViewPager viewPager;
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initViews() {
+        List<String> titles = new ArrayList<>();
+        titles.add("推薦");
+        titles.add("分類");
+        titles.add("搜索");
+        titles.add("我的");
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(new RecommendFragment());
+        fragments.add(new RecommendFragment());
+        fragments.add(new RecommendFragment());
+        fragments.add(new RecommendFragment());
+        MainAdapter mainAdapter = new MainAdapter(getSupportFragmentManager(), titles, fragments);
+        viewPager.setAdapter(mainAdapter);
+        tabLayout.setViewPager(viewPager);
+    }
+
+    @Override
+    protected void updateData() {
+    }
+
+
+}
---
+++ app/src/main/java/com/example/tvsample/RecommendFragment.java
@@ -0,0 +1,26 @@
+package com.example.tvsample;
+
+import android.support.v4.app.Fragment;
+
+import com.example.tvsample.Base.BaseFragment;
+
+/**
+ * Created by JasonWu on 27/12/2017
+ */
+
+public class RecommendFragment extends BaseFragment {
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.fragment_recommend;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void updateData() {
+
+    }
+}
